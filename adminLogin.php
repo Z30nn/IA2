3e0a0740b2d4c27c3188ccb73e2bd7484187bef8
Z30nn <junno.elizer@gmail.com>
2025-06-23 15:55:25 +0800

    Added MFA using PHPMailer

--- adminLogin.php
+++ adminLogin.php
@@ -4,45 +4,100 @@
     error_reporting(0);
     include('includes/dbconnection.php');
 
+    // PHPMailer namespace imports
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP;
+
+    $showModal = false;
+    $errorMsg = '';
+
+    // Step 1: Handle login credentials
     if(isset($_POST['login']))
     {
         $staffId=$_POST['staffId'];
-        // $password=md5($_POST['password']);
-        $password=$_POST['password'];
-        $password = md5($password);
+        $password=md5($_POST['password']);
         $query = mysqli_query($con,"select * from tbladmin where  staffId='$staffId' && password='$password'");
         $count = mysqli_num_rows($query);
         $row = mysqli_fetch_array($query);
 
         if($count > 0)
         {
-            $_SESSION['staffId']=$row['staffId'];
-            $_SESSION['emailAddress']=$row['emailAddress'];
-            $_SESSION['firstName']=$row['firstName'];
-            $_SESSION['lastName']=$row['lastName'];
-            $_SESSION['adminTypeId']=$row['adminTypeId'];
+            // Store user info in session (but not logged in yet)
+            $_SESSION['pending_staffId']=$row['staffId'];
+            $_SESSION['pending_emailAddress']=$row['emailAddress'];
+            $_SESSION['pending_firstName']=$row['firstName'];
+            $_SESSION['pending_lastName']=$row['lastName'];
+            $_SESSION['pending_adminTypeId']=$row['adminTypeId'];
 
-            if($_SESSION['adminTypeId'] == 1) // SuperAdministrator
-            {
-                echo "<script type = \"text/javascript\">
-                window.location = (\"superAdmin/index.php\")
-                </script>";  
+            // Generate code
+            $code = rand(100000, 999999);
+            $_SESSION['mfa_code'] = $code;
+            $_SESSION['mfa_code_time'] = time();
+
+            // Send email using PHPMailer
+            require 'includes/PHPMailer/src/PHPMailer.php';
+            require 'includes/PHPMailer/src/SMTP.php';
+            require 'includes/PHPMailer/src/Exception.php';
+            
+            $mail = new PHPMailer(true);
+            try {
+                //Server settings
+                $mail->isSMTP();
+                $mail->Host       = 'smtp.gmail.com';
+                $mail->SMTPAuth   = true;
+                $mail->Username   = 'ia2studentgrading@gmail.com'; // TODO: Replace with your Gmail address
+                $mail->Password   = 'zrrw jbcl hvxd ltdw';      // TODO: Replace with your Gmail App Password
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->Port       = 587;
+
+                //Recipients
+                $mail->setFrom('ia2studentgrading@gmail.com', 'Admin Login');
+                $mail->addAddress($row['emailAddress'], $row['firstName']);
+
+                //Content
+                $mail->isHTML(false);
+                $mail->Subject = 'Your Login Verification Code';
+                $mail->Body    = "Dear {$row['firstName']},\n\nYour verification code is: $code\n\nIf you did not request this, please ignore this email.";
+
+                $mail->send();
+            } catch (Exception $e) {
+                $errorMsg = "<div class='alert alert-danger' role='alert'>Verification email could not be sent. Mailer Error: {$mail->ErrorInfo}</div>";
             }
 
-            else if($_SESSION['adminTypeId'] == 2) // Administrator
-            {
-                echo "<script type = \"text/javascript\">
-                window.location = (\"admin/index.php\")
-                </script>";  
-            }
+            $showModal = true;
         }
         else
         {
             $errorMsg = "<div class='alert alert-danger' role='alert'>Invalid Username/Password!</div>";
         }
     }
-  ?>
 
+    // Step 2: Handle MFA code verification
+    if(isset($_POST['verify_code'])) {
+        $input_code = $_POST['mfa_code'];
+        if(isset($_SESSION['mfa_code']) && $input_code == $_SESSION['mfa_code'] && time() - $_SESSION['mfa_code_time'] < 600) { // 10 min expiry
+            // Move pending session vars to actual login
+            $_SESSION['staffId'] = $_SESSION['pending_staffId'];
+            $_SESSION['emailAddress'] = $_SESSION['pending_emailAddress'];
+            $_SESSION['firstName'] = $_SESSION['pending_firstName'];
+            $_SESSION['lastName'] = $_SESSION['pending_lastName'];
+            $_SESSION['adminTypeId'] = $_SESSION['pending_adminTypeId'];
+            // Clean up
+            unset($_SESSION['pending_staffId'], $_SESSION['pending_emailAddress'], $_SESSION['pending_firstName'], $_SESSION['pending_lastName'], $_SESSION['pending_adminTypeId'], $_SESSION['mfa_code'], $_SESSION['mfa_code_time']);
+            // Redirect
+            if($_SESSION['adminTypeId'] == 1) {
+                echo "<script>window.location = ('superAdmin/index.php')</script>";
+            } else if($_SESSION['adminTypeId'] == 2) {
+                echo "<script>window.location = ('admin/index.php')</script>";
+            }
+            exit();
+        } else {
+            $errorMsg = "<div class='alert alert-danger' role='alert'>Invalid or expired code. Please try again.";
+            $showModal = true;
+        }
+    }
+?>
 
 <!doctype html>
 <!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
@@ -100,24 +155,32 @@
                             </label>
                         </div>
                         <br>
-						<!-- Log on to freeprojectscodes.com for more projects! -->
                         <button type="submit" name="login" class="btn btn-success btn-flat m-b-30 m-t-30">Log in</button>
-						
-						
-						
-                        <!-- <div class="social-login-content">
-                            <div class="social-button">
-                                <button type="button" class="btn social facebook btn-flat btn-addon mb-3"><i class="ti-facebook"></i>Sign in with facebook</button>
-                                <button type="button" class="btn social twitter btn-flat btn-addon mt-2"><i class="ti-twitter"></i>Sign in with twitter</button>
-                            </div>
-                        </div> -->
-                        <!-- <div class="register-link m-t-15 text-center">
-                            <p>Don't have account ? <a href="#"> Sign Up Here</a></p>
-                        </div> -->
                     </form>
                 </div>
             </div><!-- Log on to freeprojectscodes.com for more projects! -->
         </div>
+    </div>
+
+    <!-- MFA Modal -->
+    <div class="modal" id="mfaModal" tabindex="-1" role="dialog" aria-labelledby="mfaModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="mfaModalLabel">Multi-Factor Authentication</h5>
+          </div>
+          <div class="modal-body">
+            <p>A verification code has been sent to your email. Please enter it below:</p>
+            <form method="post" id="mfaForm">
+              <div class="form-group">
+                <label for="mfa_code">Verification Code</label>
+                <input type="text" class="form-control" id="mfa_code" name="mfa_code" required maxlength="6" pattern="[0-9]{6}">
+              </div>
+              <button type="submit" name="verify_code" class="btn btn-primary">Verify</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
@@ -125,6 +188,15 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
     <script src="assets/js/main.js"></script>
+
+    <script>
+    $(document).ready(function() {
+        var showModal = <?php echo $showModal ? 'true' : 'false'; ?>;
+        if(showModal) {
+            $('#mfaModal').modal({backdrop: 'static', keyboard: false});
+        }
+    });
+    </script>
 
 </body>
 </html>
